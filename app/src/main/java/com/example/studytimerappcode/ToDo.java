7c547226bf2a26d1c0ff2768c42b83f5bf46231a
Cheșa David <140747134+ChesaDavid@users.noreply.github.com>
2024-07-29 18:52:46 +0300

    next steps todo study calendar and rest + database

--- app/src/main/java/com/example/studytimerappcode/ToDo.java
+++ app/src/main/java/com/example/studytimerappcode/ToDo.java
@@ -1,6 +1,10 @@
 package com.example.studytimerappcode;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,6 +20,9 @@ public class ToDo extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private FirebaseUser user;
     private FirebaseDatabase data;
+    private LinearLayout addTask;
+    private TextView add;
+    private Button abort,addBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,5 +34,34 @@ public class ToDo extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+//        for(int i = 0; i < 10; i++)
+        addTask = findViewById(R.id.addContainer);
+        add =  findViewById(R.id.plus);
+        abort = findViewById(R.id.buttonAbort);
+        addBtn = findViewById(R.id.buttonAdd);
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addTask.setVisibility(View.VISIBLE);
+            }
+        });
+        if(addTask.getVisibility() == View.VISIBLE){
+            abort.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    addTask.setVisibility(View.GONE);
+                }
+            });
+            addBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                }
+        });
+    }
+    }
+    public void addTaskButtonPressed(){
+
     }
 }
+//I have to make the todo app connect with firebase show what I have in the database as tasks and make the function to add
